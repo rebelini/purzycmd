600b86a104c469ebfab87dbe36ee77627b2b02a6
rebelini <zomailol132@gmail.com>
2025-03-10 04:04:45 -0700

    Add files via upload

---
+++ Purzy.bat
@@ -0,0 +1,35 @@
+@echo off
+cls
+chcp 65001 >nul
+color 0a
+title Purzy Multi-Tool
+cd files
+goto :banner
+
+:banner
+echo.
+echo.
+echo                          ██████╗ ██╗   ██╗██████╗ ███████╗██╗   ██╗      ██████╗███╗   ███╗██████╗ 
+echo                          ██╔══██╗██║   ██║██╔══██╗╚══███╔╝╚██╗ ██╔╝     ██╔════╝████╗ ████║██╔══██╗
+echo                          ██████╔╝██║   ██║██████╔╝  ███╔╝  ╚████╔╝█████╗██║     ██╔████╔██║██║  ██║
+echo                          ██╔═══╝ ██║   ██║██╔══██╗ ███╔╝    ╚██╔╝ ╚════╝██║     ██║╚██╔╝██║██║  ██║  
+echo                          ██║     ╚██████╔╝██║  ██║███████╗   ██║        ╚██████╗██║ ╚═╝ ██║██████╔╝
+echo                          ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝   ╚═╝         ╚═════╝╚═╝     ╚═╝╚═════╝    
+echo.
+echo.
+
+echo                                                             COMMANDS BELOW! 
+echo arpspoof
+echo bat2exe
+echo bypass
+echo ftp-sploit
+echo game
+echo IPToolKit
+echo main (breacher)
+echo pokebatch
+echo psexec
+echo wifipuller
+echo bat2exe
+echo Password_Breacher
+echo PortScanner
+echo PassWordChanger
---
+++ readme.txt
@@ -0,0 +1,7 @@
+installation guide: 
+
+extract "files.zip" and make sure its a normal folder file after extraction, 
+make sure all the files are in the same folder together.
+
+
+please dont rip/republish my work, my discord is rebelini, please ask for my permission before uploading my content.
